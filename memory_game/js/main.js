a93e9ab7889bd96d74efb5813017ead77cd62334
Nathanael Fletcher <nazislayer@gmail.com>
2020-01-16 11:31:10 -0500

    created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,22 +3,20 @@ let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = []
 
-let cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne + ".");
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo + ".");
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
-	} else {
-		alert("Sorry, try again");
-	}
-} 
+		} else {
+			alert("Sorry, try again");
+		};
+}
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
