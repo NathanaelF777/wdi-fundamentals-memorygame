335b94fad0d75050f28a03e7e97be7605b2ce9d4
Nathanael Fletcher <nathanaelfletcher@Nathanaels-MacBook-Pro.local>
2020-01-17 13:12:57 -0500

    Finished game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,24 +23,62 @@ let cards = [
 ];
 let cardsInPlay = []
 
-function checkForMatch() {
+//function checkForMatch() {
+	//if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		//alert("You found a match!");
+		//} else if (cardsInPlay.length === 2) {
+			//alert("Sorry, try again");
+		//};
+//}
+
+function flipCard() {
+	let id = this.getAttribute('data-id');
+	console.log("User flipped " + cards[id].rank + ".");
+	cardsInPlay.push(cards[id].rank);
+	this.setAttribute('src', cards[id].cardImage);
 	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+		//resetBoard();
 		} else if (cardsInPlay.length === 2) {
 			alert("Sorry, try again");
-		};
+			//resetBoard();
+		};	console.log(cards[id].suit);
+	console.log(cards[id].cardImage);
+};
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+		console.log(cardElement);
+	};
+};
+
+//function resetBoard() {
+	//let flipped = document.getElementById('game-board').getElementsByTagName('img');
+	//let removeFlipped =
+	//while (flipped) {
+		//flipped
+	//}
+	//for (let i = 0; i < flipped.length; i++) {
+		//flipped[i].setAttribute('src', 'images/back.png');
+//	};
+//let cardsInPlay = []
+//};
+
+function refresh() {
+		window.location.reload()
 }
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank + ".");
-	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-};
+let button = document.getElementById('Reset');
+button.addEventListener('click', refresh);
+
+createBoard();
 
 
 
 //test: flipCard(0);
 //test: flipCard(2);
-
